88b6f3f3f2d53220772ba730c9409e10cd535edf
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-22 20:39:13 +1100

    finalized the etch a sketch minimum viable product

--- Foundations/Odin-Projects/etch-a-sketch/javascript.js
+++ Foundations/Odin-Projects/etch-a-sketch/javascript.js
@@ -1,12 +1,15 @@
 // Get elements
 const sketch = document.getElementById('sketch');
+const grid16 = document.getElementById('grid16');
+const grid32 = document.getElementById('grid32');
+const eraser = document.getElementById('eraser');
+const clearButton = document.getElementById('clear-button');
+const currentColor = document.getElementById('current-color');
 
-// Variables
-let held = false;
-let gridSize = 16;
+// Grid setup
+let background = 'white';
 let color = 'black';
 
-// Functions
 function createPixelGrid(height, width) {
     for (let i = 1; i < height; i++) {
         let row = document.createElement('div');
@@ -20,18 +23,50 @@ function createPixelGrid(height, width) {
     }
 }
 
+function destroyPixelGrid() {
+    let rows = document.querySelectorAll('.row');
+    rows.forEach(row => row.remove());
+}
+
 function draw(event) {
     if (event.target.className === 'pixel') {
         event.target.style.backgroundColor = color;
     }
 }
 
-// Pointer hold handling
+// Drawing
+let held = false;
 sketch.addEventListener('pointerdown', () => held = true);
+sketch.addEventListener('pointerleave', () => held = false);
+sketch.addEventListener('pointerdown', (event) => draw(event));
 sketch.addEventListener('pointerover', event => {
     if (held === true) draw(event);
 });
-sketch.addEventListener('pointerup', () => held = false);
 
 // Initialize grid
-createPixelGrid(gridSize, gridSize);
+createPixelGrid(16, 16);
+
+// Eraser handler
+eraser.addEventListener('click', () => color = background);
+
+// Clear grid
+clearButton.addEventListener('click', () => {
+    let pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => pixel.style.backgroundColor = background);
+});
+
+// Current color handler
+currentColor.addEventListener('click', () => {
+    color = 'black';
+});
+
+// Grid size handler
+grid16.addEventListener('click', () => {
+    destroyPixelGrid();
+    createPixelGrid(16, 16);
+});
+
+grid32.addEventListener('click', () => {
+    destroyPixelGrid();
+    createPixelGrid(32, 32);
+});
