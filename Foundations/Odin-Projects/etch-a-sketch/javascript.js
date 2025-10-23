807974efba70834ab0e0b0c4d9e353314f15f89c
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-24 00:38:50 +1100

    completed the etch a sketch project to my liking, maybe i will come back some day to add a download button for the sketch

--- Foundations/Odin-Projects/etch-a-sketch/javascript.js
+++ Foundations/Odin-Projects/etch-a-sketch/javascript.js
@@ -1,24 +1,28 @@
 // Get elements
 const sketch = document.getElementById('sketch');
+const grid8 = document.getElementById('grid8');
 const grid16 = document.getElementById('grid16');
-const grid32 = document.getElementById('grid32');
 const eraser = document.getElementById('eraser');
 const clearButton = document.getElementById('clear-button');
 const colorsMenu = document.getElementById('colors-menu');
+const customSize = document.getElementById('custom-size');
 
-// Grid initialization setup
-let background = 'white';
-let color = 'black';
+// Initialization
+let gridSize = 8;
+let background = '#ffffff';
+let color = '#000000';
+document.querySelectorAll('.color-picker').forEach(item => item.style.backgroundColor = item.value);
+
 
 // Main functions
-function createPixelGrid(height, width) {
-    for (let i = 1; i <= height; i++) {
+function createPixelGrid(size) {
+    for (let i = 1; i <= size; i++) {
         let row = document.createElement('div');
         row.className = 'row';
-        for (let j = 1; j <= width; j++) {
+        for (let j = 1; j <= size; j++) {
             let pixel = document.createElement('div');
             pixel.className = 'pixel';
-            pixel.style.backgroundColor = 'white';
+            pixel.style.backgroundColor = background;
             row.appendChild(pixel);
         }
         sketch.appendChild(row);
@@ -26,8 +30,7 @@ function createPixelGrid(height, width) {
 }
 
 function destroyPixelGrid() {
-    let rows = document.querySelectorAll('.row');
-    rows.forEach(row => row.remove());
+    sketch.innerHTML = '';
 }
 
 function draw(event) {
@@ -37,59 +40,70 @@ function draw(event) {
 }
 
 // Eraser handler
-eraser.addEventListener('click', () => color = background);
+eraser.addEventListener('click', () => {
+    color = background
+    eraser.style.backgroundColor = '#ff0000';
+    eraser.style.color = '#ffffff';
+});
 
 // Clear grid
 clearButton.addEventListener('click', () => {
-    let pixels = document.querySelectorAll('.pixel');
-    pixels.forEach(pixel => pixel.style.backgroundColor = background);
+    resetGrid(gridSize);
+    color = '#000000';
+    eraser.style.backgroundColor = '#ffffff';
+    eraser.style.color = '#000000';
 });
 
 // Drawing
 let held = false;
-sketch.addEventListener('pointerdown', () => held = true);
-sketch.addEventListener('pointerup', () => held = false);
+sketch.addEventListener('pointerdown', (event) => {
+    held = true;
+    draw(event);
+});
+window.addEventListener('pointerup', () => held = false);
 sketch.addEventListener('pointerleave', () => held = false);
-sketch.addEventListener('pointerdown', (event) => draw(event));
 sketch.addEventListener('pointerover', event => {
     if (held === true) draw(event);
 });
 
 // Initialize grid
-createPixelGrid(16, 16);
+createPixelGrid(gridSize);
 
 // Grid size handler
-function setGridSize(size) {
+function resetGrid(size) {
+    gridSize = size;
     destroyPixelGrid();
-    createPixelGrid(size, size);
-}
-grid16.addEventListener('click', () => setGridSize(16));
-grid32.addEventListener('click', () => setGridSize(32));
-
-// Color picker
-const firstColor = document.getElementById('first-color');
-const secondColor = document.getElementById('second-color');
-const thirdColor = document.getElementById('third-color');
-
-function pickColor(element) {
-    element.addEventListener('click', (event) => {
-        let hidden = document.createElement('input');
-        let chosenColor = event.target.style.backgroundColor;
-        hidden.className = 'color-picker';
-        hidden.type = 'color';
-        hidden.click();
-        hidden.addEventListener('input', () => {
-            chosenColor = hidden.value
-            element.style.backgroundColor = chosenColor;
-            color = chosenColor;
-        });
-    });
+    createPixelGrid(gridSize);
+    color = '#000000';
+    eraser.style.backgroundColor = '#ffffff';
+    eraser.style.color = '#000000';
+    console.log('reset');
 }
 
-colorsMenu.addEventListener('click', (event) => {
-    color = event.target.style.backgroundColor;
+grid8.addEventListener('click', () => resetGrid(8));
+grid16.addEventListener('click', () => resetGrid(16));
+
+customSize.addEventListener('input', (event) => {
+    let userInput = parseInt(event.target.value);
+    if (Number.isInteger(userInput) && userInput >= 2 && userInput <= 100) {
+        gridSize = userInput;
+        resetGrid(userInput);
+    }
 });
 
-pickColor(firstColor);
-pickColor(secondColor);
-pickColor(thirdColor);
+// Color picker
+colorsMenu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('color-picker')) {
+        color = event.target.value
+        eraser.style.backgroundColor = '#ffffff';
+        eraser.style.color = '#000000';
+    };
+});
+
+colorsMenu.addEventListener('input', (event) => {
+    if (event.target.classList.contains('color-picker')) {
+        color = event.target.value;
+        console.log(color);
+        event.target.style.backgroundColor = color;
+    }
+});
