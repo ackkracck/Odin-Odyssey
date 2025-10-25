eb7c2e85c45e7f418181c8337c3d3ac1ebc78a31
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-25 20:43:42 +1100

    formatted the aesthetics of the screen displaying numbers

---
+++ Foundations/Odin-Projects/calculator/calculator.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+
+function operate(a, operator, b) {
+    switch (operator) {
+        case 'add':
+            add(a, b);
+            break;
+        case 'subtract':
+            subtract(a, b);
+            break;
+        case 'multiply':
+            multiply(a, b);
+            break;
+        case 'divide':
+            divide(a, b);
+            break;
+    }
+}
