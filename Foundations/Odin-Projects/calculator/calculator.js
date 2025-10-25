78268d697e061dc49ccacbdff1301a70fd8ee593
Arnold M2 Macbook Air <54977051+ackkracck@users.noreply.github.com>
2025-10-26 01:55:55 +1100

    completed basic calculations nothing else yet

--- Foundations/Odin-Projects/calculator/calculator.js
+++ Foundations/Odin-Projects/calculator/calculator.js
@@ -18,17 +18,67 @@ function divide(a, b) {
 
 function operate(a, operator, b) {
     switch (operator) {
-        case 'add':
-            add(a, b);
-            break;
-        case 'subtract':
-            subtract(a, b);
-            break;
-        case 'multiply':
-            multiply(a, b);
-            break;
-        case 'divide':
-            divide(a, b);
-            break;
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return 'Error: unknown operator';
     }
 }
+
+
+// I want to...
+// get the selected value
+// get the operator
+// get the other value
+// display the result
+
+let state = 'a';
+let input = '';
+let a = '';
+let operator = '';
+let b = '';
+let output;
+let calculated;
+let current;
+
+const userInput = document.getElementById('input');
+const keypad = document.getElementById('keypad');
+const history = document.getElementById('history');
+
+keypad.addEventListener('click', (event) => {
+
+    let target = event.target;
+    current = target.value;
+
+    if (state === 'a' && target.classList.contains('digit')) {
+        a += target.value;
+        console.log(a);
+    } else if (target.classList.contains('operator')) {
+        state = 'operate';
+        operator = target.value;
+        console.log(operator);
+    } else if (state === 'operate' && target.classList.contains('digit')) {
+        b += target.value;
+        console.log(b);
+    } else if (target.id === 'equals') {
+        calculated = operate(Number(a), operator, Number(b));
+        console.log(calculated);
+        a = calculated;
+        b = '';
+        operator = '';
+        state = 'operate';
+    }
+
+    if (target.classList.contains('digit')) {
+        userInput.textContent = target.value;
+    }
+
+    history.textContent = a + operator + b;
+
+});
